810b8132af8f74e25dc0c34de0dae119a171c182
AlcheMystic <pashkinms86@gmail.com>
2025-01-18 01:28:15 +0700

    Task5 complete!

--- crud_functions.py
+++ crud_functions.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+DEFAULT_BALANCE = 1000
+
 def initiate_db():
     connection = sqlite3.connect('telegram.db')
     cursor = connection.cursor()
@@ -9,6 +11,15 @@ def initiate_db():
         title TEXT NOT NULL,
         description TEXT,
         price INTEGER NOT NULL
+    )                
+    ''')
+    
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL
     )                
     ''')
     connection.commit()
@@ -40,7 +51,30 @@ def get_all_products():
     connection.close()
     return result
 
+def add_user(username, email, age):
+    connection = sqlite3.connect('telegram.db')
+    cursor = connection.cursor()
+    
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', (username, email, age, DEFAULT_BALANCE ))
+        
+    connection.commit()
+    connection.close()
+
+def is_included(username):
+    connection = sqlite3.connect('telegram.db')
+    cursor = connection.cursor()
+    
+    cursor.execute(f'SELECT id FROM Users WHERE username = ?', (username,))
+    result = cursor.fetchall()
+         
+    connection.commit()
+    connection.close()
+    if result == []:
+        return False
+    else:
+        return True
+    
 if __name__ == "__main__":
     initiate_db()
+    print(is_included(''))
     
-    print(get_all_products())
